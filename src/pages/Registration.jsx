cf10e5640162d4cc58657c5cdd3119a370f92607
Sajib Khan <sajibkhan.mern@gmail.com>
2023-11-08 21:10:29 +0600

    MUI frameWork setup

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
+import { Container } from '@mui/material';
 
 const Registration = () => {
   return (
-    <div>Registration</div>
+        <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <div>This is one </div>
+        </Grid>
+        <Grid item xs={6}>
+          <div>This is one </div>
+        </Grid>
+      </Grid>
   )
 }
 
