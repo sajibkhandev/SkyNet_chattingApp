46d294fc7a54e702bed715dc9d58438e8d5350a3
Sajib Khan <sajibkhan.mern@gmail.com>
2023-11-08 23:27:34 +0600

    registration page done

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,15 +1,58 @@
 import React from 'react'
+
+import registrationImg from '../assets/registrationImg.png'
+
 import Grid from '@mui/material/Grid';
-import { Container } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { alpha, styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const MyInput = styled(TextField) ({
+    width: '70%',
+    marginBottom:"34px"
+    
+  });
+  const MyButton = styled(Button)({
+    width: '70%',
+    padding:'15px 0px',
+    borderRadius:"86px",
+    background:"#5F35F5",
+    fontSize:'20px',
+    fontFamily:"Nunito",
+    fontWeight:"600",
+    marginBottom:"35px",
+    textTransform:"capitalize"
+  
+  });
 
 const Registration = () => {
   return (
-        <Grid container spacing={2}>
+        <Grid container >
         <Grid item xs={6}>
-          <div>This is one </div>
+          <div className='regBox'>
+            <h1 className='heading'>Get started with easily register</h1>
+            <p className='para'>Free register and you can enjoy it</p>
+
+            <div>
+            <MyInput id="outlined-basic" label="Email Address" variant="outlined" />
+            </div>
+            <div>
+            <MyInput id="outlined-basic" label="Ful name" variant="outlined" />
+            </div>
+            <div>
+            <MyInput type='password' id="outlined-basic" label="Password" variant="outlined" />
+            </div>
+            <MyButton variant="contained">Sign up</MyButton>
+            <p className='paraTwo'>Already  have an account ? <span>Sign In</span></p>
+           
+            
+
+          </div>
         </Grid>
         <Grid item xs={6}>
-          <div>This is one </div>
+          <div>
+            <img className='regImage' src={registrationImg} alt="registrationImg" />
+          </div>
         </Grid>
       </Grid>
   )
