f03325c31d73f8e08b5f171001b327b7d983e09c
sajibkhandev <sajibkhan.mern@gmail.com>
2024-01-10 20:34:42 +0600

    block friend

--- src/components/BlockedUsers.jsx
+++ src/components/BlockedUsers.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '@mui/material/Button';
 import userProfile1 from '../assets/userProfile1.png'
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { CiSearch } from "react-icons/ci";
+import { getDatabase, onValue, push, ref, remove, set } from "firebase/database";
+import { useSelector } from 'react-redux';
+
 
 const BlockedUsers = () => {
+     const db = getDatabase();
+    let data=useSelector((state)=>state.sajib.value)
+    let [block,setblock]=useState([])
+
+    useEffect(()=>{
+         const blockRef = ref(db, 'block/');
+         onValue(blockRef, (snapshot) => {
+            let arr=[]
+         snapshot.forEach(item=>{
+            if(item.val().blockbyId==data.uid){
+                arr.push({
+                    id:item.key,
+                    block:item.val().block,
+                    blockId:item.val().blockId
+
+                })
+            }else if(item.val().blockId=data.uid){
+                arr.push({
+                    id:item.key,
+                    blockby:item.val().blockby,
+                    blockbyId:item.val().blockbyId
+
+                })
+            }
+            
+        })
+        setblock(arr)
+         
+    });
+
+    },[])
+    
   return (
     <>
     {/* Search portion */}
@@ -21,53 +56,27 @@ const BlockedUsers = () => {
         </div>
        <div className='scroll'>
         {/* more user */}
-            <div className='main'>
+        {
+            block.map(item=>(
+                 <div className='main'>
                     <div className='pain'>
                     <img src={userProfile1} alt=""  className='userProfileCommon'/>
                     <div>
-                        <h5>Friends Reunion</h5>
+                        <h5>{item.block}</h5>
+                        <h5>{item.blockby}</h5>
                         <p className='time'>Today, 8:56pm</p>
                     </div>
                     </div>
                     <Button className='button button2' variant="contained">unblock</Button>
             </div>
+
+            ))
+        }
+           
         {/* more user */}
-        {/* more user */}
-            <div className='main'>
-                    <div className='pain'>
-                    <img src={userProfile1} alt=""  className='userProfileCommon'/>
-                    <div>
-                        <h5>Friends Reunion</h5>
-                        <p className='time'>Today, 8:56pm</p>
-                    </div>
-                    </div>
-                    <Button className='button button2' variant="contained">unblock</Button>
-            </div>
-        {/* more user */}
-        {/* more user */}
-            <div className='main'>
-                    <div className='pain'>
-                    <img src={userProfile1} alt=""  className='userProfileCommon'/>
-                    <div>
-                        <h5>Friends Reunion</h5>
-                        <p className='time'>Today, 8:56pm</p>
-                    </div>
-                    </div>
-                    <Button className='button button2' variant="contained">unblock</Button>
-            </div>
-        {/* more user */}
-        {/* more user */}
-            <div className='main'>
-                    <div className='pain'>
-                    <img src={userProfile1} alt=""  className='userProfileCommon'/>
-                    <div>
-                        <h5>Friends Reunion</h5>
-                        <p className='time'>Today, 8:56pm</p>
-                    </div>
-                    </div>
-                    <Button className='button button2' variant="contained">unblock</Button>
-            </div>
-        {/* more user */}
+       
+      
+       
        </div>
 
     </div>
