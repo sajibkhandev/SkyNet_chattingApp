e24b22f65aaa65fcd82e1f51c4426516ee908507
sajibkhandev <sajibkhan.mern@gmail.com>
2024-01-15 21:13:10 +0600

    chat box dising

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Grid from '@mui/material/Grid';
 import Group from '../components/Group';
 import Friend from '../components/Friend';
+import { BsThreeDotsVertical } from "react-icons/bs";
+import userProfile1 from '../assets/userProfile1.png'
 
 
 const Message = () => {
@@ -12,6 +14,31 @@ const Message = () => {
           <div className='heightFixed'>
           <Group/>
           <Friend/>
+          </div>
+        </Grid>
+        
+        <Grid item xs={4}>
+          <div className='heightFixed'>
+          <div className='chatBox'>
+            {/* profile start */}
+            <div className='profile'>
+              <img src="" alt="" />
+              <div>
+                <h4>Swathi </h4>
+                <p>active</p>
+              </div>
+              <BsThreeDotsVertical  className=''/>
+              
+            </div>
+            {/* profile end */}
+            {/* message start */}
+            <div className='messageBox'>
+            
+            </div>
+            {/* message end */}
+            
+
+          </div>
           </div>
         </Grid>
        
